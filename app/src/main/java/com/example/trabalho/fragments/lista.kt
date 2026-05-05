d2398118c1efdab869389c6196af7fa3867a8162
TSaulo <tsaulo.dev@gmail.com>
2026-05-05 20:54:11 -0300

    Mudança na receita de bolo

--- app/src/main/java/com/example/trabalho/fragments/lista.kt
+++ app/src/main/java/com/example/trabalho/fragments/lista.kt
@@ -34,35 +34,35 @@ class ListaFragment : Fragment() {
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
 
-        // Lista de receitas com Utensílios e Imagem (usando ícone padrão como exemplo)
+        // Lista de receitas utilizando as imagens específicas da pasta drawable
         val todasReceitas = listOf(
             Receita(
                 1, "Bolo de Cenoura", "Média", 
-                "Cenoura, Ovos, Farinha, Açúcar, Óleo",
-                "1. Bata a cenoura, ovos e óleo no liquidificador.\n2. Misture a farinha e o açúcar.\n3. Asse por 40 min.",
-                "Liquidificador, Forma de Bolo, Tigela grande",
-                android.R.drawable.ic_menu_gallery, true
+                "1/2 Xícara (chá) de Óleo\n3 Cenouras Médias Raladas\n4 ovos\n2 xícaras (chá) de açúcar\n1/2 xícaras (chá) de farinha de trigo, 1 colher (sopa) de fermento em pó",
+                "1. Em um liquidificador, adicione a cenoura, os ovos e o óleo, depois misture.\n2. Acrescente o açúcar e bata novamente por 5 minutos.\n3. Em uma tigela ou na batedeira, adicione a farinha de trigo e depois misture novamente.\n4. Acrescente o fermento e misture lentamente com uma colher.\n5. Asse em um forno preaquecido a 180° C por aproximadamente 40 minutos.\n 6.Sirva!",
+                "Liquidificador, Forma de Bolo, Batedeira",
+                R.drawable.bolocenoura, true
             ),
             Receita(
                 2, "Omelete de Queijo", "Fácil", 
                 "2 Ovos, Sal, Queijo Muçarela", 
                 "1. Bata os ovos com sal.\n2. Despeje na frigideira.\n3. Adicione o queijo e dobre.",
                 "Frigideira antiaderente, Garfo, Tigela",
-                android.R.drawable.ic_menu_gallery, true
+                R.drawable.omelete, true
             ),
             Receita(
                 3, "Lasanha à Bolonhesa", "Difícil", 
                 "Massa de Lasanha, Molho à Bolonhesa, Queijo, Presunto", 
                 "1. Prepare o molho.\n2. Monte as camadas em um refratário.\n3. Leve ao forno até gratinar.",
                 "Refratário de vidro, Panela, Forno",
-                android.R.drawable.ic_menu_gallery, false
+                R.drawable.lasanha, false
             ),
             Receita(
                 4, "Suco de Laranja", "Fácil", 
                 "4 Laranjas, Gelo", 
                 "1. Esprema as laranjas.\n2. Coe se desejar.\n3. Sirva com gelo.",
                 "Espremedor de frutas, Jarra, Faca",
-                android.R.drawable.ic_menu_gallery, false
+                R.drawable.suco, false
             )
         )
 
